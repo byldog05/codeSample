4a2c912d9a9522ac1514d98b6a0010d66265712c
Vsevolod Ban <vsevolodban@vsevolods-macbook-pro.local>
2018-10-21 20:09:35 +0300

    handled delegate with Default Image

--- Vsevolod Ban/Modules/Main Screen/User Interface/MainViewController.swift
+++ Vsevolod Ban/Modules/Main Screen/User Interface/MainViewController.swift
@@ -51,5 +51,16 @@ extension MainViewController: UITableViewDelegate {
 
 extension MainViewController: UITableViewDataSource {
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    
     
 }
