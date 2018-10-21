57f43d587b647820cdd3a57ff8447477150ab2ed
Vsevolod Ban <vsevolodban@vsevolods-macbook-pro.local>
2018-10-21 18:00:47 +0300

    Initial Commit

---
+++ Vsevolod Ban/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vsevolod Ban
+//
+//  Created by Vsevolod Ban on 10/21/18.
+//  Copyright © 2018 Vsevolod Ban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
